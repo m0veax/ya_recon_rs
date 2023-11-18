d056b106b64b7f6d17b8c0a69bdef51d4b284677
m0veax <patrick.kilter@gmail.com>
2023-11-18 10:17:47 +0100

    reading env variables to determine what we want to do

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm just trying to learn rust here. I recently started playing CTFs and thought about automating some tasks.
+
+I don't know where this will end up, but we will see.
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use std::env;
+
+// Reading arguments to determin what to do
+
+struct EnvArgs{
+    selected_module: String,
+    selected_mode: String,
+    target: String
+}
+
+/**
+TODO use clap crate
+
+return EnvArgs
+**/
+fn read_args() -> EnvArgs {
+    // Get the command-line arguments.
+    let args: Vec<String> = env::args().collect();
+
+    // Check if at least two arguments are provided (the first argument is the program name).
+    if args.len() < 4 {
+        eprintln!("Usage: {} <module> <mode> <target>", args[0]);
+        std::process::exit(1);
+    }
+
+    // Get the first and second arguments (index 1 and 2 in the `args` vector).
+    
+    let selected_mode = args[1].clone();
+    let selected_module = args[2].clone();
+    let target = args[3].clone();
+
+    // Now, you can work with the `selected_module` and `target` as needed.
+    println!("The first argument is: {}", selected_module);
+
+    // Create an instance of the `EnvArgs` struct and return it.
+    EnvArgs {
+        selected_module,
+        selected_mode,
+        target,
+    }
+}
+
+
+fn whitelist_modes(mode: &str) -> bool {
+    let modes = vec!["nmap"];
+    
+    modes.contains(&mode)
+    
+}
+
+
+fn main() {
+    
+    println!("Start ...");
+    let args = read_args();
+
+    println!( "Selected Module: {}", args.selected_module);
+    println!( "Selected Mode: {}", args.selected_mode);
+    println!( "Target: {}", args.target);
+
+    if whitelist_modes(&args.selected_mode) {
+        println!("Found Mode: {}", args.selected_mode);
+    }
+}
